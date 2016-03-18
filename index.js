2b9c5870886ae3c14a7bb667f4a7e6793abae5e9
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2016-03-18 17:34:46 -0500

    Voy en el Video No 26

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function(){
+	var header = document.getElementById('app-header')
+	var seleccion = $([document , header]);
+	
+	$('p').filter('.text') //selecciona todos los <p class="text"></p>
+})
+
+
+VAMOS EN VIDEO OBJETOS DE JQUERY MINUTO 20
+
